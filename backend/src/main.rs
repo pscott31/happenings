6479b9b2786de9c2bdecd9949ea2d4755e023629
Philip Scott <phil@vega.xyz>
2023-11-22 16:19:58 +0000

    chore: add signal handler, tidy

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,14 @@
+use std::{sync::mpsc, thread};
+
 use app::*;
 use axum::{routing::post, Router};
 use dotenv::dotenv;
 use fileserv::file_and_error_handler;
 use leptos::*;
 use leptos_axum::{generate_route_list, LeptosRoutes};
-use log::{info, LevelFilter};
+use log::{info, warn, LevelFilter};
+use nix::sys::signal::Signal;
+use signal_hook::{consts::SIGINT, iterator::Signals};
 
 pub mod fileserv;
 
@@ -31,12 +35,34 @@ async fn main() {
         .fallback(file_and_error_handler)
         .with_state(leptos_options);
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
     log::info!("listening on http://{}", &addr);
-    axum::Server::bind(&addr)
+
+    // Channel for graceful shutdown
+    let (tx, rx) = mpsc::channel();
+    let mut signals = Signals::new(&[SIGINT]).unwrap();
+
+    thread::spawn(move || {
+        for sig_num in signals.forever() {
+            let signal_name = Signal::try_from(sig_num)
+                .map(|s| format!("{:?}", s))
+                .unwrap_or_else(|_| format!("unknown signal({})", sig_num));
+
+            info!("received {:?}", signal_name);
+            tx.send(()).unwrap();
+        }
+    });
+    let server = axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .with_graceful_shutdown(async {
+            match rx.recv() {
+                Ok(_) => info!("starting graceful shutdown"),
+                Err(err) => warn!("error on shutdown channel: {:?}", err),
+            }
+        });
+
+    if let Err(e) = server.await {
+        log::error!("server error: {}", e);
+    }
+    log::info!("graceful shutdown complete");
 }
 
